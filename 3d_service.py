b50cf90f636bc921de8dba17cbd5aa29cc223c60
RJdeck <rockchen4@gmail.com>
2022-12-28 16:43:00 +0800

    fix: pack into class

--- 3d_service.py
+++ 3d_service.py
@@ -1,3 +1,12 @@
+import torch
+from tqdm.auto import tqdm
+
+from point_e.diffusion.configs import DIFFUSION_CONFIGS, diffusion_from_config
+from point_e.diffusion.sampler import PointCloudSampler
+from point_e.models.download import load_checkpoint
+from point_e.models.configs import MODEL_CONFIGS, model_from_config
+from point_e.util.plotting import plot_point_cloud
+
 class meta3d_model():
     def __init__(self):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -37,8 +46,8 @@ class meta3d_model():
         self.model_diffusion = diffusion_from_config(DIFFUSION_CONFIGS[self.base_name])
         self.unsampler_diffusion = diffusion_from_config(DIFFUSION_CONFIGS['upsample'])
 
-    # create a function to create the point cloud sampler
-    def create_point_cloud_sampler(self):
+    # create a function to do the generation
+    def generate(self, prompt: str = 'a red motorcycle'):
         sampler = PointCloudSampler(
             device=self.device,
             models=[self.base_model_loaded, self.unsampler_diffusion_loaded],
@@ -48,13 +57,9 @@ class meta3d_model():
             guidance_scale=[3.0, 0.0],
             model_kwargs_key_filter=('texts', ''), # Do not condition the upsampler at all
         )
-        return sampler
-    
-    # create a function to do the generation
-    def generate(self, prompt: str = 'a red motorcycle'):
         # Produce a sample from the model.
         samples = None
-        for x in tqdm(self.create_point_cloud_sampler().sample_batch_progressive(batch_size=1, model_kwargs=dict(texts=[prompt]))):
+        for x in tqdm(sampler.sample_batch_progressive(batch_size=1, model_kwargs=dict(texts=[prompt]))):
             samples = x
         pc = self.create_point_cloud_sampler().output_to_point_clouds(samples)[0]
         return pc   
@@ -64,4 +69,12 @@ class meta3d_model():
         save the model to ply file
         '''
         with open(filename, 'wb') as f:
-            self.generate().weite_ply(f)
+            self.generate().write_ply(f)
+
+if __name__ == '__main__':
+    # create the model
+    model = meta3d_model()
+    model.create_model()
+    model.load_model()
+    model.get_diffusion()
+    pc2 =  model.generate()
