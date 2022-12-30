f0e545865f8229f0a3cd3a5c46cc2a608d2fdeb9
RJdeck <rockchen4@gmail.com>
2022-12-30 18:14:34 +0800

    fix: class fixed

--- meta3d/__tests__/test_meta_3d_service.py
+++ meta3d/__tests__/test_meta_3d_service.py
@@ -1,4 +1,4 @@
-from meta3d.services.meta_3d_service import Meta3dService
+from meta3d.services.meta_3d_service import Meta3dService, PointEService
 from unittest import TestCase
 
 class MockS3Service:
@@ -11,12 +11,22 @@ class MockS3Service:
     
     def check_exists(self, file_name: str):
         return self.should_file_exist
+
+
+class MockPointEService(PointEService):
+    def load_checkpoint(self, base_name, device):
+        return None, None
+
+    def model_from_config(self, model_config, device) -> PointEService.Model:
+        model = PointEService.Model()
+        return model
         
 
 class TestMeta3dService(TestCase):
     def setUp(self):
         self.mock_s3_service_true = MockS3Service(True)
         self.mock_s3_service_false = MockS3Service(False)
+        self.mock_pointe_service = MockPointEService()
 
     def test_check_model_ture(self):
         service = Meta3dService(s3_service=self.mock_s3_service_true)
@@ -26,6 +36,9 @@ class TestMeta3dService(TestCase):
     def test_check_model_false(self):
         service = Meta3dService(s3_service=self.mock_s3_service_false)
         service.check_model('D:')
-        self.assertEqual(self.mock_s3_service_false.counter, 2)
-    # run the test in gitbash terminal
-    # python -m pytest
+        self.assertEqual(2, self.mock_s3_service_false.counter)
+
+    def test_create_model(self):
+        service = Meta3dService(pointe_service=self.mock_pointe_service)
+        service.create_model("")
+        self.assertEqual(1, 1)
--- meta3d/services/meta_3d_service.py
+++ meta3d/services/meta_3d_service.py
@@ -1,3 +1,4 @@
+import point_e.models.configs
 from regex import D
 import torch
 from tqdm.auto import tqdm
@@ -13,26 +14,45 @@ from point_e.models.configs import MODEL_CONFIGS, model_from_config
 from meta3d.common import config
 from meta3d.services.s3_service import download_file
 
+
 class MachineLearningService:
     def save(self, model, save_path):
         torch.save(model, save_path)
+
     def load(self, device, model_path):
         return torch.load(model_path, map_location=device)
+
 
 class S3Service:
     def download_file(self, file_path, bucket, object_name):
         s3 = boto3.client('s3')
         s3.download_file(bucket, object_name, file_path)
-    
+
     def check_exists(self, file_name: str):
         return os.path.exists(file_name)
 
 
+class PointEService:
+    class Model:
+        def eval(self):
+            pass
+
+        def load_state_dict(self, val):
+            pass
+
+    def model_from_config(self, model_config, device) -> Model:
+        return point_e.models.configs.model_from_config(model_config, device)
+
+    def load_checkpoint(self, base_name, device):
+        return load_checkpoint(base_name, device)
+
+
 class Meta3dService:
-    def __init__(self, ml_service = MachineLearningService(), s3_service = S3Service()):
-        self.ml_service = ml_service
+    def __init__(self, pointe_service=PointEService(), s3_service=S3Service(), ml_service=MachineLearningService()):
+        self.pointe_service = pointe_service
         self.s3_service = s3_service
-    
+        self.ml_service = ml_service
+
     def save_model(self, base_model, upsampler_model, model_path: str):
         '''
         save the model
@@ -42,7 +62,18 @@ class Meta3dService:
 
         self.ml_service.save(base_model, base_model_path)
         self.ml_service.save(upsampler_model, unsample_model_path)
-    
+
+    def load_model(self, device, model_path: str):
+        '''
+        load the model
+        '''
+        base_model_path = model_path + 'base_model.pt'
+        unsample_model_path = model_path + 'upsample_model.pt'
+
+        base_model_loaded = torch.load(base_model_path, map_location=device)
+        unsampler_model_loaded = torch.load(unsample_model_path, map_location=device)
+        return base_model_loaded, unsampler_model_loaded
+
     def check_model(self, model_path: str):
         '''
         check if the model exists in the path
@@ -50,44 +81,31 @@ class Meta3dService:
         base_model_path = model_path + 'base_model.pt'
         unsample_model_path = model_path + 'upsample_model.pt'
 
-        if not os.path.exists(base_model_path):
+        if not self.s3_service.check_exists(base_model_path):
             s3_b_model_path = config.BUCKET_model_folder + 'cup.ply'
-            download_file(base_model_path, config.BUCKET_NAME, s3_b_model_path)
-            
-        if not os.path.exists(unsample_model_path):
+            self.s3_service.download_file(base_model_path, config.BUCKET_NAME, s3_b_model_path)
+
+        if not self.s3_service.check_exists(unsample_model_path):
             s3_un_model_path = config.BUCKET_model_folder + 'upsample_model.pt'
             print('no such file: ' + unsample_model_path)
-            download_file(unsample_model_path, config.BUCKET_NAME, s3_un_model_path)
+            self.s3_service.download_file(unsample_model_path, config.BUCKET_NAME, s3_un_model_path)
 
+    def create_model(self, device, base_name: str = 'base40M-textvec'):
+        """
+        create the model
+        """
+        base_model = self.pointe_service.model_from_config(model_config=MODEL_CONFIGS[base_name], device=device)
+        base_model.eval()
 
-def create_model(device, base_name: str = 'base40M-textvec'):
-    """
-    create the model
-    """
-    base_model = model_from_config(MODEL_CONFIGS[base_name], device)
-    base_model.eval()
+        upsampler_model = self.pointe_service.model_from_config(MODEL_CONFIGS['upsample'], device)
+        upsampler_model.eval()
 
-    upsampler_model = model_from_config(MODEL_CONFIGS['upsample'], device)
-    upsampler_model.eval()
+        base_model.load_state_dict(self.pointe_service.load_checkpoint(base_name, device))
 
-    base_model.load_state_dict(load_checkpoint(base_name, device))
+        upsampler_model.load_state_dict(self.pointe_service.load_checkpoint('upsample', device))
 
-    upsampler_model.load_state_dict(load_checkpoint('upsample', device))
+        return base_model, upsampler_model
 
-    return base_model, upsampler_model
-
-
-
-def load_model(device, model_path: str):
-    '''
-    load the model
-    '''
-    base_model_path = model_path + 'base_model.pt'
-    unsample_model_path = model_path + 'upsample_model.pt'
-
-    base_model_loaded = torch.load(base_model_path, map_location=device)
-    unsampler_diffusion_loaded = torch.load(unsample_model_path, map_location=device)
-    return base_model_loaded, unsampler_diffusion_loaded
 
 def create_diffusion(base_name: str):
     """
@@ -96,6 +114,7 @@ def create_diffusion(base_name: str):
     base_diffusion = diffusion_from_config(DIFFUSION_CONFIGS[base_name])
     upsampler_diffusion = diffusion_from_config(DIFFUSION_CONFIGS['upsample'])
     return base_diffusion, upsampler_diffusion
+
 
 def generate_3d_result(device, base_model, upsampler_model, base_diffusion, upsampler_diffusion, prompt: str):
     """
@@ -108,8 +127,8 @@ def generate_3d_result(device, base_model, upsampler_model, base_diffusion, upsa
         num_points=[1024, 4096 - 1024],
         aux_channels=['R', 'G', 'B'],
         guidance_scale=[3.0, 0.0],
-        model_kwargs_key_filter=('texts', ''), # Do not condition the upsampler at all
-        )
+        model_kwargs_key_filter=('texts', ''),  # Do not condition the upsampler at all
+    )
     samples = None
     for x in tqdm(sampler.sample_batch_progressive(batch_size=1, model_kwargs=dict(texts=[prompt]))):
         samples = x
@@ -117,7 +136,8 @@ def generate_3d_result(device, base_model, upsampler_model, base_diffusion, upsa
     pc = sampler.output_to_point_clouds(samples)[0]
     return pc
 
-def save_model2ply(model ,ply_path: str):
+
+def save_model2ply(model, ply_path: str):
     '''
     save the model to ply file
     '''
