b93d79fc2ff9124ee74a28fb3b52941926a607d7
RJdeck <rockchen4@gmail.com>
2023-01-04 17:50:42 +0800

    fix: del config

--- meta3d/common/config.py
+++
@@ -1,8 +0,0 @@
-from dotenv import dotenv_values
-import os
-
-_config = dotenv_values(".env")
-
-BUCKET_NAME = os.getenv("BUCKET_NAME", "meta3d-bucket")
-BUCKET_model_folder = os.getenv("BUCKET_model_folder", "meta3d-bucket")
-REGION = os.getenv("REGION", "ap-east-1231")
