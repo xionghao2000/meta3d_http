f164176d1c74a271b1ead9d5ba1834a14581be88
RJdeck <rockchen4@gmail.com>
2023-01-10 11:32:27 +0800

    fix: check error fix

--- meta3d/services/meta_3d_service.py
+++ meta3d/services/meta_3d_service.py
@@ -103,13 +103,13 @@ class Meta3dService:
 
         if not self.s3_service.check_exists(base_model_path):
             s3_b_model_path = self.config.BUCKET_model_folder + 'base_model.pt'
-            self.s3_service.download_file(file_name=base_model_path, bucketname=self.config.BUCKET_NAME,
+            self.s3_service.download_file(file_path=base_model_path, bucket=self.config.BUCKET_NAME,
                                           object_name=s3_b_model_path)
 
         if not self.s3_service.check_exists(unsample_model_path):
             s3_un_model_path = self.config.BUCKET_model_folder + 'upsample_model.pt'
             print('no such file: ' + unsample_model_path)
-            self.s3_service.download_file(file_name=unsample_model_path, bucketname=self.config.BUCKET_NAME,
+            self.s3_service.download_file(file_path=unsample_model_path, bucket=self.config.BUCKET_NAME,
                                           object_name=s3_un_model_path)
 
     def create_model(self, device, base_name: str = 'base40M-textvec'):
